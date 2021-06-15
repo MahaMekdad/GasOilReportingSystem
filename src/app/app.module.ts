c2f37ef8f1fa66e5ee02a696da55a8a567cae1bd
MahaMekdad <maha_mekdad@outlook.com>
2021-06-15 23:28:39 +0200

    fluidLevelMeasurement table retrieve and record delete + routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { TestComponent } from './components/test/test.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
@@ -5,10 +6,14 @@ import { ApiModule } from 'api.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FluidLevelMeasurementsComponent } from './components/fluid-level-measurements/fluid-level-measurements.component';
 
 @NgModule({
+  
   declarations: [
-    AppComponent
+    AppComponent,
+    FluidLevelMeasurementsComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
