a49b72f513da7645a422d750209ac474572ddc09
basiony <mohamed.wafa770@gmail.com>
2021-06-19 22:42:18 +0200

    adding the Concession, Well Test tables Management functionality
    Creating components for the above mentioned entities and managing routing as well as crud operations

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,35 @@
-import { NgModule } from '@angular/core';
+import { TestEditComponent } from './components/test-edit/test-edit.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {ProductionBudgetComponent} from './components/production-budget/production-budget.component';
+import { CommonModule } from '@angular/common';
+
+import { ProdDashboardComponent } from './components/prod-dashboard/prod-dashboard.component';
+import { ConcessionComponent } from './components/concession/concession.component';
+import { WellTestTableComponent } from './components/well-test-table/well-test-table.component';
+import { ProductionBudgetComponent } from './components/production-budget/production-budget.component';
 import { FluidLevelMeasurementsComponent } from './components/fluid-level-measurements/fluid-level-measurements.component';
 import { TestComponent } from './components/test/test.component';
 
 const routes: Routes = [
-  {
-    path: 'flm',
-    component: FluidLevelMeasurementsComponent
-  },
-  {
-    path: 'test',
-    component: TestComponent
-  },
-  {path: 'productionbudget', component: ProductionBudgetComponent}
+  { path: 'home', component: ProdDashboardComponent },
+  { path: 'dashboard', component: ProdDashboardComponent },
+  { path: 'concessions', component: ConcessionComponent },
+  { path: 'tests', component: WellTestTableComponent },
+  { path: 'flm', component: FluidLevelMeasurementsComponent },
+  { path: 'test', component: TestComponent },
+  { path: 'productionbudget', component: ProductionBudgetComponent },
+  { path: 'login', component: ProdDashboardComponent },
+  { path: 'registration', component: ProdDashboardComponent },
+  { path: 'test', component: ProdDashboardComponent },
+  { path: 'productionbudget', component: ProductionBudgetComponent },
+  { path: 'test/add/:id', component: TestEditComponent },
+  { path: 'test/edit/:id', component: TestEditComponent }
+
 ];
 
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule,RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
