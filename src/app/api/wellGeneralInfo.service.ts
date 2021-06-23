65f32bc293ea1fa1243ff7bf0c14595a2ad1a14d
MahaMekdad <maha_mekdad@outlook.com>
2021-06-23 20:58:13 +0200

    testing and adding the components to the well-tabs component

--- src/app/api/wellGeneralInfo.service.ts
+++ src/app/api/wellGeneralInfo.service.ts
@@ -28,7 +28,7 @@ import { Configuration }                                     from 'service_utils
 @Injectable()
 export class WellGeneralInfoService {
 
-    protected basePath = 'http://localhost:8080';
+    protected basePath = 'http://localhost:8000';
     public defaultHeaders = new HttpHeaders();
     public configuration = new Configuration();
 
